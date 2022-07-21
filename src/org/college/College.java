83023398dfa643a907e37f6f95ecc0a5f35ef3b8
ArulRubiyal <arulruiyal@gmail.com>
2022-07-21 15:29:52 +0530

    Kavintask

--- src/org/college/College.java
+++ src/org/college/College.java
@@ -5,15 +5,19 @@ public class College {
 		System.out.println("College Name is GCT");
 
 	}
+
 	public void collegeCode() {
-        System.out.println("College Code is 007");
-        
+		System.out.println("College Code is 007");
 
 	}
+	
+	private void collegeAdd() {
+		System.out.println("COllege Address is ECR chennai");
+	}
+
 	public void collegeRank() {
 		System.out.println("College Rank is 7");
 
 	}
-	
 
 }
